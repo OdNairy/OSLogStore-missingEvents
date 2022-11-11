3314ebb9f52c4ff8b472db76bcd5c8998b9af1e6
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-11-11 15:56:05 +0200

    Initial Commit

---
+++ SingpostStoreSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingpostStoreSample
+//
+//  Created by odnairy on 11.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
